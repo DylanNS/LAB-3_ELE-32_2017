c6d6f5340995dfa828d82f7cd1ffd1f96e114ba2
DylanNS <dylan-ns@hotmail.com>
2017-10-25 17:31:03 -0200

    funcoes para calcular a probabilidade de erro

--- CanalBSC.py
+++ CanalBSC.py
@@ -10,6 +10,7 @@ from bitarray import bitarray
 from copy import deepcopy
 from numpy import array
 from numpy import array_equal
+from math import pow
 
 def CanalBSC(vetorEntrada, p):
     
@@ -20,7 +21,7 @@ def CanalBSC(vetorEntrada, p):
     for pos in posicao:
         vetorSaida[pos] ^= True
     
-    return vetorSaida
+    return vetorSaida,quantMuda
 
 vetorEntrada = bitarray(10)
 print("vetorEntrada: ",vetorEntrada,"vetorSaida: ",CanalBSC(vetorEntrada,0.5))
@@ -75,15 +76,97 @@ def decodificadorHamming(r):
     if(not array_equal(s,array([0,0,0]))):
         erro = MapErro(s)
     else:
-        print("Erro! Sindrome eh Vetor Nulo!")
+        erro = array([0,0,0,0,0,0,0])
     saida = r+ erro
     for i in range(len(saida)):
         
         saida[i] %=2
     return saida
 
-r = CanalBSC(vetorCodificado,0.2)
+r,quantMudado = CanalBSC(vetorCodificado,0.2)
 print("VEtor recebido: ", r)
 estimativa = decodificadorHamming(r)
 print("Vetor estimativa do transmitido: ", estimativa)
 
+def RandomNumberGenerator(quantVetores):
+    
+    dic = {} #guarda array que tem 0 e 1
+    #comeca em zero e vai ate 10^6/4 -1
+    for i in range(quantVetores):
+        dic[i] = random.randint(0,2,4)
+    return dic
+quantVetores = int(pow(10,6)/4)
+dic = RandomNumberGenerator(quantVetores)
+print("Dic: ", len(dic))
+filename = "amostraOriginal.txt"
+
+
+def writeFile(filename,dic):
+    escritor = open(filename,'w',encoding = 'utf-8')
+    i = 1
+    for index in dic:
+        escritor.write(str(i)+" ---> ")
+        escritor.write(str(dic.get(index)))
+        escritor.write("\n")
+        i+=1
+    escritor.close()
+    
+#writeFile(filename,dic)
+
+def PassarNoCanalBSC(dic,p):
+    
+    resposta = {}
+    quantMudado = 0
+    for key in dic:
+        
+      resposta[key],mudou = CanalBSC(dic.get(key),p)
+      quantMudado +=mudou
+      
+    return resposta,quantMudado
+p=0.5
+amostraAlterada,quantMudado = PassarNoCanalBSC(dic,p)
+print("Tamanho amostra Alterada: ", len(amostraAlterada))
+print("Quantidade de bits mudado: ",quantMudado)
+print("Porcentagem de erro sem codificacao: ",quantMudado/pow(10,6))
+filename = "amostraAlterada.txt"
+#writeFile(filename,amostraAlterada)
+def PassarNoCodificador(dic):
+    
+    amostraCodificada = {}
+    for key in dic:
+        
+        amostraCodificada[key] = codificadorHamming(dic.get(key))
+    return amostraCodificada
+
+amostraCodificada = PassarNoCodificador(dic)
+print("Tamanho amostra Codificada: ",len(amostraCodificada))
+p=0.2
+amostraCodificadaAlterada,quantMudado = PassarNoCanalBSC(amostraCodificada,p)
+print("Tamanho amostra Alterada: ", len(amostraCodificadaAlterada))
+print("Quantidade de bits mudado: ",quantMudado)
+print("Porcentagem de erro com codificacao: ",quantMudado/pow(10,6))
+
+def PassarNoCodificador(dic):
+    
+    amostraDecodificada = {}
+    for key in dic:
+        
+        amostraDecodificada[key] = decodificadorHamming(dic.get(key))
+    return  amostraDecodificada
+
+amostraDecodificada = PassarNoCodificador(amostraCodificadaAlterada)
+print("Tamanho amostra Decodificada: ",len(amostraDecodificada))
+
+def ContarErros(decodificada,original):
+    
+    quantErro =0
+    for i in range(len(original)):
+        for j in range(len(original.get(1))):
+            if(not decodificada[i][j] == original[i][j]):
+                quantErro +=1
+    return quantErro
+
+quantErro = ContarErros(amostraDecodificada,amostraCodificada)
+print("Quantidade de erro: ",quantErro)
+print("Porcentagem de erro: ",quantErro/pow(10,6))
+print("Porcentagem de erro corrigido: ", (quantMudado-quantErro)/quantMudado)
